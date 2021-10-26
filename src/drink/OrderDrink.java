d12a2fc6937ec3d17bdb27ff7f73465aa91adba3
AlexNik13 <4pnav21@gmail.com>
2021-10-26 15:07:05 +0300

    Drinks +

--- src/drink/OrderDrink.java
+++ src/drink/OrderDrink.java
@@ -6,10 +6,20 @@ import java.util.ArrayList;
 
 public class OrderDrink implements Order {
     private ArrayList<Drink> drinks = new ArrayList<>();
+    private double costPizza;
+    private double cost;
 
+    public OrderDrink(ArrayList<Drink> drinks) {
+        this.drinks = drinks;
+    }
 
     @Override
     public void printCheck() {
-
+        System.out.println("Напитки: ");
+        for (Drink drink : drinks){
+            System.out.printf("%s (%s) - %.2f\n", drink.getName(), drink.getPortion() ,drink.getCost());
+            cost += drink.getCost();
+        }
+        System.out.printf("    за напитки %.2f\n", cost);
     }
 }
--- src/pizza/OrderPizza.java
+++ src/pizza/OrderPizza.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class OrderPizza implements Order {
     ArrayList<Pizza> pizza;
     private double costPizza;
-    double cost;
+    private double cost;
 
     public OrderPizza(ArrayList<Pizza> pizza) {
         this.pizza = pizza;
@@ -16,12 +16,11 @@ public class OrderPizza implements Order {
 
     @Override
     public void printCheck() {
-        System.out.println("Чек");
+        System.out.println("Пицца: ");
         for (int i = 0; i < pizza.size(); i++) {
             System.out.printf("%s - %.2f\n", pizza.get(i).getName() ,pizza.get(i).getCost());
             cost += pizza.get(i).getCost();
         }
-        System.out.printf("Итого %.2f\n", cost);
-
+        System.out.printf("    за пиццу %.2f\n", cost);
     }
 }
